2bffd110cbab9fdb2be519b9b90dd4b7ee5a553f
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-09-29 15:25:06 +0200

    Update Project.java

--- Labb3b/model/Project.java
+++ Labb3b/model/Project.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,6 +21,44 @@ public class Project implements Comparable<Project>, Serializable {
         listOfTasks = new ArrayList<Task>();
         nextTaskId = 1;
     }
+    public String getTitle() {
+        return title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public int getNextTaskId() {
+        return nextTaskId;
+    }
+    /*public LocalDate getLastUpdated() {
+
+    }*/
+    public ArrayList<Task> getListOfTasks() {
+        ArrayList<Task> temp = new ArrayList<>();
+        for(int i=0;i< listOfTasks.size();i++) {
+            temp.add(i,listOfTasks.get(i));
+        }
+        return temp;
+    }
+    public ArrayList<Task> findTasks(ITaskMatcher matcher) {
+        ArrayList<Task> tmp = new ArrayList<>();
+        for (int i=0;i< listOfTasks.size();i++) {
+            if(matcher.match(listOfTasks.get(i))) {
+                tmp.add(i,listOfTasks.get(i));
+            }
+        }
+     return tmp;
+    }
     public Task addTask(String descr, Prio prio) {
         Task task = new Task(descr,prio,nextTaskId);
         listOfTasks.add(task);
@@ -37,5 +76,16 @@ public class Project implements Comparable<Project>, Serializable {
     @Override
     public int compareTo(Project o) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                ", nextTaskId=" + nextTaskId +
+                '}';
     }
 }
