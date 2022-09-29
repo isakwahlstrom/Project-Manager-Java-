c744deb65b206040e51a5055295b6f48dbe52868
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-09-29 10:02:13 +0200

    Delete src directory

--- src/model/Main.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Main {
-    public static void main(String[] args) {
-        Task task = new Task<>("Do homework",Prio.High, 1234);
-        System.out.println(task.toString());
-    }
-}
