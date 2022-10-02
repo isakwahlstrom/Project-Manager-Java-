cd55dffa1349dd589058fe8d295e628d5162dc41
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-10-02 21:36:50 +0200

    Update Main.java

--- Labb3b/model/Main.java
+++ Labb3b/model/Main.java
@@ -2,7 +2,20 @@ package model;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new Task<>("Do homework",Prio.High, 1234);
-        System.out.println(task.toString());
+        Task task = new Task("Do Homework",Prio.Low, 1);
+        Task task2 = new Task("Do Homework",Prio.High, 2);
+        //task.setTakenBy("Karl");
+        //System.out.println(task.toString());
+       Project project = new Project("Projekt 2022","Vi bygger en mikroprocessor",1);
+        project.addTask("tada",Prio.Low);
+        project.addTask("tada",Prio.High);
+        project.addTask("tada",Prio.Medium);
+        //System.out.println(task.compareTo(task2));
+
+       System.out.println(project.findTasks(new NotDoneMatcher()));
+        System.out.println(project.getLastUpdated());
+
+
+
     }
 }
