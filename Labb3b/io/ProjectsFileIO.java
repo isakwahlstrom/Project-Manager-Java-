9a162813b0e591c48d7f8eb9f70fd08e929b253c
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-10-05 18:22:37 +0200

    Update ProjectsFileIO.java

--- Labb3b/io/ProjectsFileIO.java
+++ Labb3b/io/ProjectsFileIO.java
@@ -5,15 +5,14 @@ import java.io.*;
 import java.util.List;
 
 
- /** Hints on how to implement serialization and deserialization
+ /** Implementation of serialization and deserialization
  * of lists of projects and users.
-*/
+  * */
 public class ProjectsFileIO {
-
     /*
      * Call this method before the application exits, to store the users and projects,
      * in serialized form.
-*/
+    */
     public static void serializeToFile(File file, List<Project> data) throws IOException {
         ObjectOutputStream out = null;
         try {
@@ -29,8 +28,7 @@ public class ProjectsFileIO {
     /*
      * Call this method at startup of the application, to deserialize the users and
      * from file the specified file.
-*/
-   // @SuppressWarnings("unchecked")
+    */
     public static List<Project> deSerializeFromFile(File file) throws IOException, ClassNotFoundException {
         ObjectInputStream in = null;
         try {
