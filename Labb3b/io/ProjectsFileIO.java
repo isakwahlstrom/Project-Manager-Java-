d522627115a7342ec0aa5bd8b6b342a90d13435c
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-10-05 09:10:49 +0200

    Update ProjectsFileIO.java

--- Labb3b/io/ProjectsFileIO.java
+++ Labb3b/io/ProjectsFileIO.java
@@ -1,36 +1,53 @@
-/*package io;
+package io;
 
 import model.Project;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Hints on how to implement serialization and deserialization
- * of lists of projects and users.
 
+ /** Hints on how to implement serialization and deserialization
+ * of lists of projects and users.
+*/
 public class ProjectsFileIO {
 
-    /**
+    /*
      * Call this method before the application exits, to store the users and projects,
      * in serialized form.
-
+*/
     public static void serializeToFile(File file, List<Project> data) throws IOException {
-        // ...
-        // and then, make sure the file always get closed
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(file));
+            out.writeObject(data);
+        } finally {
+            if(out!=null) {
+                out.close();
+            }
+        }
+
     }
 
-    /**
+    /*
      * Call this method at startup of the application, to deserialize the users and
      * from file the specified file.
-
-    @SuppressWarnings("unchecked")
-    public static List<Project> deSerializeFromFile(File file) throws IOException, ClassNotFoundException {
-        // ...
-        // and then, make sure the file always get closed
+*/
+   // @SuppressWarnings("unchecked")
+    public static ArrayList<Project> deSerializeFromFile(File file) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new FileInputStream(file));
+            ArrayList<Project> projects = (ArrayList<Project>) in.readObject();
+            return projects;
+        } finally {
+            if(in!=null) {
+                in.close();
+            }
+        }
     }
+
 
     private ProjectsFileIO() {}
 }
 
-*/
