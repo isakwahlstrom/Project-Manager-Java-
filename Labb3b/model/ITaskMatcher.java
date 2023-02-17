c527850920710c50df1998e342fca508890817bb
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2023-02-17 07:03:24 +0100

    Delete Labb3b directory

--- Labb3b/model/ITaskMatcher.java
+++
@@ -1,7 +0,0 @@
-package model.matcher;
-
-import model.Task;
-
-public interface ITaskMatcher {
- public boolean match(Task task);
-}
--- Labb3b/model/Prio.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Prio {
-    Low, Medium, High
-}
